6da4ebe29b373ba1aea9d60b3b333fbd7eaa2ba4
Tomiicosta <tomi11costa@gmail.com>
2025-10-31 18:02:01 -0300

    Diseño Login/Register casi terminados, ajustes en header y main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { TicketStep1 } from './pages/ticket-step1/ticket-step1';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'billboard', component: Home},
     {path: 'cinemas', component: Home},
     {path: 'login', component: Login},
+    {path: 'register', component: Register},
     {path: 'ticket/step1', component: TicketStep1}
 ];
