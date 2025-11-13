654a863161b57b1c4950ccbb5873069effc6e47d
ilgazzolo <luchogazzolo@gmail.com>
2025-11-13 02:05:13 -0300

    html step 4 y PaymentService

--- src/app/services/payment/payment-service.ts
+++ src/app/services/payment/payment-service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare var MercadoPago: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagoService {
+
+  // Base API
+  private apiUrl = 'http://localhost:8080/api/payments';
+
+  // Credencial MP
+  private mp : string | undefined;
+
+  constructor(private http: HttpClient) {
+    // Se inicializa una sola vez
+    this.mp = new MercadoPago('APP_USR-dd489abe-d5ed-4a90-a939-efe24534a5ca', {
+      locale: 'es-AR'
+    });
+  }
+
   
+  /**
+   * Inicializa el SDK de Mercado Pago en el frontend
+   * (solo si el script fue cargado correctamente en index.html)
+   */
+  inicializarMercadoPago(): any {
+    return this.mp;
+  }
+
+
+  /**
+   * Llama al backend para crear una preferencia de pago
+   */
+  crearPreferencia(payload: any) {
+    return this.http.post(`${this.apiUrl}/create`, payload);
+  }
+
+
 }
+
